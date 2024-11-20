91aec279ac3cbed2c2b7caf64811e8acf2c6cc09
Yvan Kalalo <kalaloyvan07@gmail.com>
2024-11-20 11:25:20 +0800

    My Third Commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,7 @@ python manage.py migrate
 
 # Start the Django server
 # Using `runserver` in development; change to `gunicorn` or other WSGI server for production
-python manage.py runserver 0.0.0.0:8000 &
+# python manage.py runserver 0.0.0.0:8000 &
+
+exec gunicorn mysite.wsgi:application --bind 0.0.0.0:8000 --workers 3
+
