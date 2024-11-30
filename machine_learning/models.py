69f21253cc71909a4f49a9e2818865b07a799164
Yvan Kalalo <kalaloyvan07@gmail.com>
2024-11-30 16:37:45 +0800

    My Thirty Six Commit

--- machine_learning/models.py
+++ machine_learning/models.py
@@ -42,7 +42,6 @@ class UserProgress(models.Model):
     progress = models.PositiveIntegerField()  # Percentage or count of completed workouts
     date = models.DateField(auto_now_add=True)
     progress_date = models.DateField(auto_now=True)  # Automatically update the date whenever the progress is updated
-    is_completed = models.BooleanField(default=False)
     
     def __str__(self):
         return f"{self.user.username} - {self.workout.Title} - {self.progress}%"
