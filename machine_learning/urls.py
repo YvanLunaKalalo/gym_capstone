e25d79a79e22ecc113006f1a2987c52a9e07011d
Yvan Kalalo <kalaloyvan07@gmail.com>
2024-12-03 01:44:36 +0800

    My Thirty Eight Commit

--- machine_learning/urls.py
+++ machine_learning/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('recommend/', views.workout_recommendation_view, name="recommend"),
     path('workout-recommendations/', views.workout_recommendation_view, name='workout_recommendations'),
     # path('progress/<str:workout_title>/', views.update_progress_view, name='update_progress'),
-    path('workout-session/', views.workout_session_view, name='workout_session'),
-    path('workout-dashboard/', views.workout_dashboard_view, name='workout_dashboard'),
+    path('workout_session/<str:workout_title>/', views.workout_session_view, name='workout_session'),
+    path('workout_dashboard/', views.workout_dashboard_view, name='workout_dashboard'),
 ]
