0f0258cf12cabef651d89c21cd9ad7710fb614b3
Yvan Kalalo <kalaloyvan07@gmail.com>
2024-12-19 23:33:18 +0800

    My Second Revision Commit - Debugging in Workout Session - URLS.py

--- machine_learning/urls.py
+++ machine_learning/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('bmi/', views.bmi_view, name="bmi"),
     path('recommend/', views.workout_recommendation_view, name="recommend"),
-    path('workout/session/', views.workout_session_view, name='workout_session'),
-    path('workout/next/', views.next_workout_view, name='next_workout'),
-    path('workout/complete/', views.workout_complete_view, name='workout_complete'),
+    path('recommend/session/', views.workout_session_view, name='workout_session'),
+    path('recommend/next/', views.next_workout_view, name='next_workout'),
+    path('recommend/complete/', views.workout_complete_view, name='workout_complete'),
     path('progress-tracker/', views.progress_tracker_view, name='progress_tracker'),
 ]
