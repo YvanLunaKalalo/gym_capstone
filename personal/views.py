0557e53abe90e4ea1a1b2cc8fb255ded6ababd53
Yvan Kalalo <kalaloyvan07@gmail.com>
2024-12-20 22:16:24 +0800

    My Third Revision Commit - Dashboard

--- personal/views.py
+++ personal/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.template import loader
 from personal.models import Contact
 from machine_learning.models import Workout, UserProfile, UserProgress
+from collections import defaultdict
 
 def index_view(request):
     template = loader.get_template('index.html')
@@ -38,16 +39,34 @@ def dashboard_view(request):
 
     # Get the user profile for the logged-in user
     user_profile = UserProfile.objects.filter(user=request.user).first()
+    
+    # Get all workouts
+    workouts = Workout.objects.all()
+    
+    # Example grouping workouts by day
+    workouts_by_day = defaultdict(list)  # Dictionary to store workouts by day
+    day = 1
+    for i, workout in enumerate(workouts):
+        if (i % 5) == 0 and i > 0:
+            day += 1
+        workouts_by_day[day].append(workout)
 
     # Fetch only the workouts and progress associated with the logged-in user
     user_progress = UserProgress.objects.filter(user=request.user)
+    
+    # Calculate total completed workouts for each day
+    user_progress_by_day = {}
+    for day, day_workouts in workouts_by_day.items():
+        completed_count = user_progress.filter(workout__in=day_workouts, completed=True).count()
+        user_progress_by_day[day] = completed_count
 
     # Filter recommended workouts from UserProgress model
     workouts = [progress.workout for progress in user_progress]
 
     context = {
         'user_profile': user_profile,
-        'workouts': workouts,
+        'workouts_by_day': workouts_by_day,
+        'user_progress_by_day': user_progress_by_day,
         'user_progress': user_progress,
     }
 
